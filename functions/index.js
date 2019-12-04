8529ff7bd713fd6bebfb7ec7b8ea8463335fae66
darinmarinov <darinmarinov@users.noreply.github.com>
2019-12-04 16:22:56 +0200

    Add functions which send notification when  we login or sign up
    This function is firebase function that add custom  data  to user collection

--- functions/index.js
+++ functions/index.js
@@ -27,3 +27,19 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
     return createNotification(notification)
  })
+
+ exports.userJoined = functions.auth.user().onCreate(
+     user => {
+         return admin.firestore().collection('users').doc(user.uid).get().then(
+             doc => {
+                 const newUser = doc.data();
+                 const notification = {
+                     content: 'Joined the party',
+                     user: `${newUser.firstName} ${newUser.lastName}`,
+                     time: admin.firestore.FieldValue.serverTimestamp()
+                 }
+                 return createNotification(notification)
+             }
+         )
+     }
+ )
